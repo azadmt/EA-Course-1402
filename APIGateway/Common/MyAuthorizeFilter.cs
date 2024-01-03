c9b91902c772e0bb5bb03726acac095067f0ec43
m.azad <m.azad@xaniis.com>
2024-01-04 00:13:50 +0330

    ---

--- APIGateway/Common/MyAuthorizeFilter.cs
+++ APIGateway/Common/MyAuthorizeFilter.cs
@@ -22,21 +22,5 @@ namespace APIGateway.Common
         }
     }
 
-    public class LogMetricFilter : ActionFilterAttribute
-    {
-        private Stopwatch stopwatch;
 
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            stopwatch = Stopwatch.StartNew();
-        }
-
-        public override void OnActionExecuted(ActionExecutedContext context)
-        {
-            stopwatch.Stop();
-            var elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
-            var message = $"Action took {elapsedMilliseconds} ms to execute.";
-            Debug.WriteLine(message);
-        }
-    }
 }
